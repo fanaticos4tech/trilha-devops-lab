33a1199883b1cc9b805aa3206ae14e3cf30221c0
fanaticos4tech <fanaticos4tech@gmail.com>
2025-05-14 18:12:40 +0200

    Create backend.tf

---
+++ terraform/environments/prod/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket  = "prod-trilha-devops-fanaticos4tech-terraform-state"
+    key     = "terraform.tfstate"
+    region  = var.aws_region
+    encrypt = true
+  }
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
